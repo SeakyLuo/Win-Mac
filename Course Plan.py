b75dcb2ceb26d139520b959d4c1df09f57ced4be
SeakyLuo <haitianluo@umail.ucsb.edu>
2018-01-18 13:57:07 -0800

    Update Course Plan.py

--- Course Plan.py
+++ Course Plan.py
@@ -3,64 +3,41 @@ from tkinter import messagebox
 import ez
 
 root=Tk()
-def overlap(time1,time2):
-    if time1==time2:
-        return True
-    t111=eval(time1[:time1.find(':')])
-    t211=eval(time2[:time2.find(':')])
-    if t111+[0,12][t111<8]>t211+[0,12][t211<8]:
-        time1,time2=time2,time1
-    t11,t12=time1.split('-')
-    t21,t22=time2.split('-')
-    if t11==t21 or t12==t22:
-        return True
-    t111,t112=t11.split(':')
-    t111=eval(t111)
-    t111+=[0,12][t111<8]
-    t112=eval(t112)
-    t121,t122=t12.split(':')
-    t121=eval(t121)
-    t121+=[0,12][t121<8]
-    t122=eval(t122)
+def evaluate(time):
+    '''Time format 12:30'''
+    colon=time.find(':')
+    h=eval(time[:colon])
+    if h<8:
+        h+=12
+    m=time[colon+1:]
+    if m[0]=='0':
+        m=eval(m[1])
+    else:
+        m=eval(m)
+    return h*60+m
 
-    t211,t212=t21.split(':')
-    t211=eval(t211)
-    t211+=[0,12][t211<8]
-    t212=eval(t212)
-    t221,t222=t22.split(':')
-    t221=eval(t221)
-    t221+=[0,12][t221<8]
-    t222=eval(t222)
-    if t111>t221 or \
-       (t111==t221 and t112>t222) or \
-       t121<t211 or \
-       (t121==t211 and t122<t212):
-        return False
-    return True
+def overlap(time1,time2):
+    t1=[evaluate(t) for t in time1.split('-')]
+    t2=[evaluate(t) for t in time2.split('-')]
+    if any(t1[0]<t<t1[1] for t in t2) or any(t2[0]<t<t2[1] for t in t1):
+        return True
+    return False
 
 def length(t1,t2):
-    '''Time format 12:30'''
-    i1=t1.index(':')
-    h1=eval(t1[:i1])
-    if h1<8:
-        h1+=12
-    time1=h1*60+eval(t1[i1+1:])
-    i2=t2.index(':')
-    h2=eval(t2[:i2])
-    if h2<8:
-        h2+=12
-    time2=h2*60+eval(t2[i2+1:])
-    return abs(time2-time1)
+    return abs(evaluate(t2)-evaluate(t1))
 
 class coursePlan(Frame):
     def __init__(self,parent):
         Frame.__init__(self,parent)
-        self.weekday={'Mon':'m','Tue':'t','Wed':'w','Thu':'r','Fri':'f'}
+        self.labelWidth=18
+        self.widgetHeight=1
+        self.aweekday=['m','t','w','r','f']
+        self.weekday=['Mon','Tue','Wed','Thu','Fri']
         self.time=['{}:{}0'.format((i//2+7)%12+1,[0,3][i%2]) for i in range(24)]
         for i,date in enumerate(self.weekday):
-            Label(self,text=date).grid(row=0,column=i+1)  ##needs bg
+            Label(self,text=date,width=self.labelWidth).grid(row=0,column=i+1)  ##needs bg
         for i,time in enumerate(self.time):
-            Label(self,text=time).grid(row=i+1,column=0)  ##needs bg
+            Label(self,text=time,height=self.widgetHeight).grid(row=i+1,column=0)  ##needs bg
         self.timeLabel=Label(self,text='Time:')
         self.timeBox=Entry(self)
         self.exampleText='Example: tr12-1:15'
@@ -77,16 +54,15 @@ class coursePlan(Frame):
             box.grid(row=i,column=7,columnspan=2)
             box.bind('<KeyRelease>',self.switch)
         self.addButton=Button(self,text='Add',command=self.add)
-        self.addButton.grid(row=3,column=6)
         self.readButton=Button(self,text='Read',command=self.read)
-        self.readButton.grid(row=3,column=7)
         self.colorButton=Button(self,text='Color',command=self.switchColor)
-        self.colorButton.grid(row=3,column=8)
         self.saveButton=Button(self,text='Save',command=self.save)
-        self.saveButton.grid(row=4,column=6)
         self.clearButton=Button(self,text='Clear',command=self.clear)
-        self.clearButton.grid(row=4,column=7)
-        self.courseListLabel=Label(self,text='Course List')
+        self.buttonList=[self.addButton,self.readButton,self.colorButton,self.saveButton,self.clearButton]
+        for i,button in enumerate(self.buttonList):
+            button['borderwidth']=0.5
+            button.grid(row=3+i//3,column=6+i%3)
+        self.courseListLabel=Label(self,text='Course List',height=self.widgetHeight)
         self.courseListLabel.grid(row=5,column=6)
         self.read()
 
@@ -96,18 +72,66 @@ class coursePlan(Frame):
         self.courseList=[]
         self.colorList=['white','pink','orange','yellow','light green','cyan','azure','violet','magenta']
         self.nameBox.focus()
+        self.exception=False
 
     def putLabel(self,datetime,name,loc,mode=0):
+        def weekIndex(datetime):
+            weekindex=0
+            while datetime[weekindex].isalpha():
+                if datetime[weekindex] not in self.aweekday:
+                    raise SyntaxError
+                weekindex+=1
+            return weekindex
         datetime=datetime.lower()
-        courseLabel=Label(self,text=name+[' --- ',''][loc==None]+loc)
+        try:
+            weekindex=weekIndex(datetime)
+            if weekindex==0:
+                raise SyntaxError
+            time=datetime[weekindex:]
+            if time.isnumeric():
+                time='{}:00-{}:50'.format(time,time)
+            elif time.count('-')==1 and time.count(':')<2:
+                dash=time.find('-')
+                start=time[:dash]
+                if start.count(':')==0:
+                    start+=':00'
+                end=time[dash:] ## 不用dash+1
+                if end.count(':')==0:
+                    end+=':00'
+                time=start+end
+            elif time.count('-')!=1 or time.count(':')>2:
+                raise SyntaxError
+            ## 时间冲突不能加课 可不可以有别的解决方法呢
+            for item in self.courses:
+                t2=item[0]
+                weekindex2=weekIndex(t2)
+                if any(weekday in datetime[:weekindex] for weekday in t2[:weekindex2]) and \
+                   overlap(time,t2[weekindex2:]):
+                    raise ValueError
+        except SyntaxError:
+            print(weekindex,time.count('-')!=1,time.count(':')>2)
+##            messagebox.showinfo('Error','Invalid Time Format!\n'+self.exampleText)
+            self.exception=True
+            return
+        except ValueError:
+            messagebox.showinfo('Error','Schedule conflict!\nCourse cannot be added.')
+##            print(item[1],name,any(weekday in datetime[:weekindex] for weekday in t2[:weekindex2]),overlap(time,t2[weekindex2:]))
+            self.exception=True
+            return
+        dash=time.find('-')
+        start=time[:dash]
+        end=time[dash+1:]
+        grids=length(start,end)//30+1
+##        datetime=datetime[:weekindex]+time
+        courseLabel=Label(self,text=name+[' --- '+loc,''][loc==''])
         index=-1
         if mode==0:
             rowNum=len(self.courseList)+6
             courseLabel.grid(row=rowNum,column=6)
-            modifyButton=Button(self,text='Modify')
+            modifyButton=Button(self,text='Modify',height=self.widgetHeight,borderwidth=0.5)
             modifyButton.grid(row=rowNum,column=7)
             modifyButton.configure(command=lambda button=modifyButton:self.modify(button))
-            dropButton=Button(self,text='Drop',command=self.drop)
+            dropButton=Button(self,text='Drop',command=self.drop,height=self.widgetHeight,borderwidth=0.5)
             dropButton.grid(row=rowNum,column=8)
             dropButton.configure(command=lambda button=dropButton:self.drop(button))
             self.courseList.append((courseLabel,modifyButton,dropButton))
@@ -121,36 +145,13 @@ class coursePlan(Frame):
             for label in self.courseLabels[index]:
                 label.grid_forget()
             self.courseLabels[index]=[]
-
-        weekindex=1
-        while datetime[weekindex].isalpha():
-            weekindex+=1
-        time=datetime[weekindex:]
-        if time.isnumeric():
-            time='{}:00-{}:50'.format(time,time)
-        elif time.count('-') and time.count(':')<2:
-            dash=time.find('-')
-            start=time[:dash]
-            if start.count(':')==0:
-                start+=':00'
-            end=time[dash:]
-            if end.count(':')==0:
-                end+=':00'
-            time=start+end
-        dash=time.find('-')
-        start=time[:dash]
-        end=time[dash+1:]
-        grids=length(start,end)//30+1
+            
         for weekday in datetime[:weekindex]:
-            day=ez.find(self.weekday).key(weekday)[0]
-            beginRow=self.time.index(start)+1
-            col=list(self.weekday.keys()).index(day)+1
-            maxlen=max(len(name),len(loc))
-            courseInfo=[('{:'+str(maxlen)+'}').format(name),('{:'+str(maxlen)+'}').format(loc)]+[' '*maxlen for i in range(grids-2)]
-            for i in range(grids):
-                label=Label(self,text=courseInfo[i],bg=self.colorList[0])
-                self.courseLabels[index].append(label) 
-                label.grid(row=beginRow+i,column=col)
+            day=self.weekday[self.aweekday.index(weekday)]
+            label=Label(self,text=name+'\n'+loc,bg=self.colorList[0],\
+                        width=self.labelWidth,height=self.widgetHeight*grids)
+            self.courseLabels[index].append(label) 
+            label.grid(row=self.time.index(start)+1,column=self.weekday.index(day)+1,rowspan=grids)
 
     def add(self,mode=0):
         datetime=self.timeBox.get()
@@ -162,7 +163,10 @@ class coursePlan(Frame):
             messagebox.showinfo('Error','Please input course name!')
             return
         self.putLabel(datetime,name,loc,mode)
-        self.clearBox()
+        if self.exception:
+            self.exception=False
+        else:
+            self.clearBox()
 
     def read(self):
         self.setup()
@@ -251,8 +255,9 @@ class coursePlan(Frame):
 
     def switchColor(self):
         self.colorList=self.colorList[1:]+[self.colorList[0]]
-        for label in self.courseLabels:
-            label.configure(bg=self.colorList[0])
+        for lst in self.courseLabels:
+            for label in lst:
+                label.configure(bg=self.colorList[0])
 
     def focusIn(self,event):
         if self.timeBox.get()==self.exampleText:
@@ -266,4 +271,5 @@ class coursePlan(Frame):
 
 cp=coursePlan(root)
 cp.pack()
+root.title('Schedule')
 root.mainloop()
