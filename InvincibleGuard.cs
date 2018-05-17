937ca06bb657c31b7781430a2917c09cef4f44fd
SeakyLuo <haitianluo@umail.ucsb.edu>
2018-05-18 06:35:52 +0800

    Add files via upload

---
+++ InvincibleGuard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvincibleGuard : Trigger {
+
+	public override void BloodThirsty ()
+	{
+		GameInfo.actions [InfoLoader.playerID]++;
+	}
+}
---
+++ LandmineEngineer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandmineEngineer : Trigger {
+
+	public override void AtEnemyBottom ()
+	{
+		GameController.PlaceTrap (piece.location, Database.RandomTrap (), InfoLoader.playerID);
+	}
+}
