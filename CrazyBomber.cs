3ccaf0d9f48ce66c969966263875a097a95388e3
SeakyLuo <haitianluo@umail.ucsb.edu>
2018-05-19 02:53:41 +0800

    Piece

---
+++ CrazyBomber.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrazyBomber : Trigger {
+
+    public override void Activate()
+    {
+        base.Activate();
+    }
+}
