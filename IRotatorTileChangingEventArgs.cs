3aabba2258198b03026f3468f4139737a855a96f
SeakyLuo <haitianluo@umail.ucsb.edu>
2020-05-29 09:45:32 +0800

    RotatorTile

---
+++ IRotatorTileChangingEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
+{
+    [ContractVersion(typeof(UniversalApiContract), 65536)]
+    [ExclusiveTo(typeof(RotatorTileChangingEventArgs))]
+    [GuidAttribute(1553488582, 55298, 19256, 162, 35, 191, 7, 12, 67, 254, 223)]
+    [WebHostHidden]
+    internal interface IRotatorTileChangingEventArgs
+    {
+        object OldItem { get; }
+        object NewItem { get; }
+    }
+}
---
+++ RotatorTileChangingEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
+{
+    [Composable(typeof(IRotatorTileChangingEventArgs), CompositionType.Public, 65536, "Windows.Foundation.UniversalApiContract")]
+    [ContractVersion(typeof(UniversalApiContract), 65536)]
+    [MarshalingBehavior(MarshalingType.Agile)]
+    [Threading(ThreadingModel.Both)]
+    [WebHostHidden]
+    public sealed class RotatorTileChangingEventArgs : IRotatorTileChangingEventArgs
+    {
+        public RotatorTileChangingEventArgs(object oldItem, object newItem)
+        {
+            OldItem = oldItem;
+            NewItem = newItem;
+        }
+        public object OldItem { get; private set; }
+        public object NewItem { get; private set; }
+    }
+
+}
